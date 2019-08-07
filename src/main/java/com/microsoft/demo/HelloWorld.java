3bfcf030cb4cc21361b6e1326c505b732e71c81e
d061059 <Alexander.Hauck@sap.com>
2019-08-08 01:26:51 +0200

    Adjust overall project settings

--- src/main/java/com/microsoft/demo/HelloWorld.java
+++ src/main/java/com/microsoft/demo/HelloWorld.java
@@ -1,14 +1,16 @@
 package com.microsoft.demo;
 
-import java.io.IOException;
+import java.io.IOException;  
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
  * Servlet implementation class HelloWorld
  */
+
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
