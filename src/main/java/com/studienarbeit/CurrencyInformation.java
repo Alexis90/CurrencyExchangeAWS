c5ffacb035b1bb4efe2347be5164223aa899bb3d
d061059 <Alexander.Hauck@sap.com>
2019-08-08 14:40:04 +0200

    Add some unit Tests

--- src/main/java/com/studienarbeit/CurrencyInformation.java
+++ src/main/java/com/studienarbeit/CurrencyInformation.java
@@ -1,31 +1,31 @@
 package com.studienarbeit;
 
-public class CurrencyInformation {
+public final class CurrencyInformation {
 	
 	private final static double euro_chf_course = 1.09;
-	private final static double  euro_dollar_course =1.12;
+	private final static double euro_dollar_course =1.12;
 	private final static double euro_pound_course = 0.92;
 	private final static double chf_euro_course = 0.91;
 	private final static double dollar_euro_course = 0.89;
 	private final static double pound_euro_course = 1.08;
 	
 	
-	protected static double getEuro_chf_course() {
+	public static double getEuro_chf_course() {
 		return euro_chf_course;
 	}
-	protected static double getEuro_dollar_course() {
+	public static double getEuro_dollar_course() {
 		return euro_dollar_course;
 	}
-	protected static double getEuro_pound_course() {
+	public static double getEuro_pound_course() {
 		return euro_pound_course;
 	}
-	protected static double getChf_euro_course() {
+	public static double getChf_euro_course() {
 		return chf_euro_course;
 	}
-	protected static double getDollar_euro_course() {
+	public static double getDollar_euro_course() {
 		return dollar_euro_course;
 	}
-	protected static double getPound_euro_course() {
+	public static double getPound_euro_course() {
 		return pound_euro_course;
 	}
 	
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+import com.studienarbeit.CurrencyInformation;
 import com.studienarbeit.Demo;
 
 public class MyTest {
@@ -10,6 +13,15 @@ public class MyTest {
     }
 
     @Test
-    public void test_method_2() {
+    public void checkCurrencyInformationValues() {
+    	
+    	CurrencyInformation currencyInformation = new CurrencyInformation(); //check whether Class is available
+    	
+    	assertEquals("Check whether euro_chf exchange value is correct", 1.09, CurrencyInformation.getEuro_chf_course(), 0);
+    	assertEquals("Check whether euro_dollar exchange value is correct", 1.12, CurrencyInformation.getEuro_dollar_course(), 0);
+    	assertEquals("Check whether euro_pound exchange value is correct", 0.92, CurrencyInformation.getEuro_pound_course(), 0);
+    	assertEquals("Check whether dollar_euro exchange value is correct", 0.89, CurrencyInformation.getDollar_euro_course(), 0);
+    	assertEquals("Check whether chf_euro exchange value is correct", 0.91, CurrencyInformation.getChf_euro_course(), 0);
+    	assertEquals("Check whether pound_euro exchange value is correct", 1.08, CurrencyInformation.getPound_euro_course(), 0);    	
     }
 }
